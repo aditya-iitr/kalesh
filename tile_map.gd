1dd1440e9a0316ade9861a51653ea06491f82309
Aditya Ranjan <ar482468@gmail.com>
2024-10-19 02:54:42 +0530

    part 1

---
+++ tile_map.gd
@@ -0,0 +1,12 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+"res://chess/pieces/archers.png"
+"res://chess/pieces/baby-dragon.png"
+"res://chess/pieces/barbarians.png"
+"res://chess/pieces/bowler.png"
+"res://chess/pieces/hog-rider.png"
+"res://chess/pieces/minions.png"
+"res://chess/pieces/wizard.png"
+"res://chess/pieces/warden.png"
